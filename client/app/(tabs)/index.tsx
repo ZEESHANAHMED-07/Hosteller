a9903fd10511efd04917212fca8721d3741a3fa7
SOUMO <soumo2020.saha@gmail.com>
2025-07-28 20:47:51 +0530

    l

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">HOSTELLER :</ThemedText>
+        <ThemedText type="title">H OSTELLER :</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
